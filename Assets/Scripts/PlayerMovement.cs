ed1b9f72f457d872d0af0c3b471aa23acfc270e9
wasabiQuoc <wasabi.together@gmail.com>
2025-02-20 20:02:44 +0700

    PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f; 
+    [SerializeField] private PlayerInput playerInput;
+
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        playerInput.neverAutoSwitchControlSchemes = true;   
+        Debug.Log(moveInput);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = moveInput * speed * Time.deltaTime;
+        rb.velocity = movement;
+    }
+}
